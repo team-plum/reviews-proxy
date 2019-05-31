0e547507260833134cac57b40f6ee2e9bdd2f0f6
Beverly Hernandez <you@example.com>
2019-05-31 14:14:07 -0400

    changed some files and changed IPs

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/:id', express.static(path.join(__dirname, 'public'))), 
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
